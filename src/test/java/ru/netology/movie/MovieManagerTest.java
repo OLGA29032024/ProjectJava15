cba935a3f05235ab48f4debce5bf34d0628382ec
Qamid <ivanova3487@gmail.com>
2024-06-08 14:05:40 +0300

    Initial commit

---
+++ src/test/java/ru/netology/movie/MovieManagerTest.java
@@ -0,0 +1,62 @@
+package ru.netology.movie;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+    @Test
+    public void addMovie() {
+        MovieManager manager = new MovieManager();
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void oneAddMovie() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Бладшот");
+
+        String[] expected = {"Бладшот"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void fourAddMovie() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперёд");
+        manager.addMovie("Отель Белград");
+        manager.addMovie("Джентельмены");
+        manager.addMovie("Человек невидимка");
+        manager.addMovie("Тролли.Мировой тур");
+        manager.addMovie("Номер один");
+
+
+        String[] expected = {"Бладшот", "Вперёд", "Отель Белград", "Джентельмены", "Человек невидимка", "Тролли.Мировой тур", "Номер один"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперёд");
+        manager.addMovie("Отель Белград");
+        manager.addMovie("Джентельмены");
+        manager.addMovie("Человек невидимка");
+        manager.addMovie("Тролли.Мировой тур");
+        manager.addMovie("Номер один");
+
+        String[] expected = {"Номер один", "Тролли.Мировой тур", "Человек невидимка", "Джентельмены", "Отель Белград", "Вперёд", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
